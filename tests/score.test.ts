33abadab51c605ce3c5bb6371358eeb33968f9be
Richard Li <richard.li@detectorinspector.com.au>
2022-03-04 15:42:06 +1100

    test

--- tests/score.test.ts
+++ tests/score.test.ts
@@ -4,13 +4,15 @@ import { Category } from '../src/Category';
 describe('score a given roll with a given category correctly', () => {
     it.each([
         { roll: [1, 2, 3, 4, 5], category: Category.Chance, expected: 15 },
+        { roll: [1, 1, 3, 3, 6], category: Category.Chance, expected: 14 },
     ])(
         'roll $roll in category $category should score $expected.',
         ({
             roll,
             category,
             expected,
-        }: { roll: number[];
+        }: {
+            roll: number[];
             category: Category;
             expected: number;
         }) => {
