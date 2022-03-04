21c6b2cc7e4ad89eb52d5f9712b2297cb29ebd41
Richard Li <richard.li@detectorinspector.com.au>
2022-03-04 15:51:38 +1100

    pass

--- src/score.ts
+++ src/score.ts
@@ -1,6 +1,14 @@
+import { allEqual } from './allEqual';
 import { Category } from './Category';
 import { sum } from './sum';
 
 export function score(roll: number[], category: Category): number {
-    return sum(roll);
+    switch (category) {
+        case Category.Chance:
+            return sum(roll);
+        case Category.Yatzy:
+            return allEqual(roll) ? 50 : 0;
+        default:
+            return 0;
+    }
 }
